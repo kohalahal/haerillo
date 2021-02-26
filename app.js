5e36cbb359c0f29189ebca8ed2052b2181638b77
Kohalahal <52988648+kohalahal@users.noreply.github.com>
2021-02-26 15:32:52 +0900

    config

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var boardRouter = require('./routes/board');
+var registerRouter = require('./routes/register');
 
 var app = express();
 
@@ -15,6 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/board', boardRouter);
+app.use('/register', registerRouter);
 
 module.exports = app;
